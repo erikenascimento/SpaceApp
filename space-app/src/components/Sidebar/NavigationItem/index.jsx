0e4e7de7e479bf714d386b6965e169fcc57c57aa
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-05-13 22:34:22 -0300

    Styling the ListItem component

--- space-app/src/components/Sidebar/NavigationItem/index.jsx
+++ space-app/src/components/Sidebar/NavigationItem/index.jsx
@@ -1,6 +1,15 @@
 import styled from "styled-components";
 
-const StyledListItem = styled.li``;
+const StyledListItem = styled.li`
+	font-size: 24px;
+	line-height: 29px;
+	margin-bottom: 30px;
+	cursor: pointer;
+	color: ${props => (props.$active ? "#7B78E5" : "#D9D9D9")};
+	display: flex;
+	align-items: center;
+	gap: 22px;
+`;
 
 const NavigationItem = ({
 	children,
@@ -9,7 +18,7 @@ const NavigationItem = ({
 	active = false,
 }) => {
 	return (
-		<StyledListItem>
+		<StyledListItem $active={active}>
 			<img src={active ? activeIcon : inactiveIcon} alt="Ícone do item" />
 			{children}
 		</StyledListItem>
