85f9745bcbdbae7ecb328cad2e6021f8b3c16d82
Erike Nascimento <nascimento.lf.erike@gmail.com>
2024-05-14 22:51:25 -0300

    Populating the tag component with a json file using the method .map

--- space-app/src/components/Gallery/Tags/index.jsx
+++ space-app/src/components/Gallery/Tags/index.jsx
@@ -1,5 +1,43 @@
+import styled from "styled-components";
+import tags from "./tags.json";
+
+const TagsContainer = styled.section`
+	display: flex;
+	align-items: center;
+	gap: 32px;
+	margin-top: 56px;
+`;
+
+const TagTitle = styled.h3`
+	color: #d9d9d9;
+	font-size: 24px;
+	margin: 0;
+`;
+
+const TagButton = styled.button`
+	font-size: 24px;
+	color: #ffffff;
+	background: rgba(217, 217, 217, 0.3);
+	border-radius: 10px;
+	cursor: pointer;
+	transition: background-color 0.3s ease;
+	padding: 12px;
+	box-sizing: border-box;
+	border: 2px solid transparent;
+	&:hover {
+		border-color: #c98cf1;
+	}
+`;
+
 const Tags = () => {
-	return <p>Tags</p>;
+	return (
+		<TagsContainer>
+			<TagTitle>Busque por tags: </TagTitle>
+			{tags.map(tag => (
+				<TagButton key={tag.id}>{tag.titulo}</TagButton>
+			))}
+		</TagsContainer>
+	);
 };
 
 export default Tags;
